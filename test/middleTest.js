205ab63eb9f1943eb4df4edabea2f25646729105
Sam <samjulian6@gmail.com>
2022-09-25 23:31:28 -0700

    Changed unit tests to use Mocha and Chai for head(), middle(), and tail()

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,23 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([4]), []);
-assertArraysEqual(middle([4, 5]), []);
-assertArraysEqual(middle([4, 5, 6]), [5]);
-assertArraysEqual(middle([4, 5, 6, 7]), [5, 6]);
-assertArraysEqual(middle([4, 5, 6, 7, 8]), [6])
+describe("#middle", () => {
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it("returns [] for [4]", () => {
+    assert.deepEqual(middle([4]), []); 
+  });
+  it("returns [] for [4, 5]", () => {
+    assert.deepEqual(middle([4, 5]), []); 
+  });
+  it("returns [5] for [4, 5, 6]", () => {
+    assert.deepEqual(middle([4, 5, 6]), [5]); 
+  });
+  it("returns [5, 6] for [4, 5, 6, 7]", () => {
+    assert.deepEqual(middle([4, 5, 6, 7]), [5, 6]); 
+  });
+  it("returns [6] for [4, 5, 6, 7, 8]", () => {
+    assert.deepEqual(middle([4, 5, 6, 7, 8]), [6]); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,17 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-assertEqual(tail([1, 2, 3]).toString(), [2, 3].toString());
-assertEqual(tail([2, 3]).toString(), [3].toString());
-assertEqual(tail([3]).toString(), [].toString());
-assertEqual(tail([]).toString(), [].toString());
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [3] for [2, 3]", () => {
+    assert.deepEqual(tail([2, 3]), [3]); 
+  });
+  it("returns [] for [3]", () => {
+    assert.deepEqual(tail([3]), []); 
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []); 
+  });
+});

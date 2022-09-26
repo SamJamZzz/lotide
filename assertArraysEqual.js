d5a2b7c0a01c0884b7f88d63e289d2c04639bdb7
Sam <samjulian6@gmail.com>
2022-09-25 22:10:43 -0700

    Refactored and modularized assertArraysEqual()

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,36 +1,8 @@
-const eqArrays = function(arr1, arr2) {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-    return false;
-  }
-
-  if (arr1.length !== arr2.length) {
-    return false;    
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-
-    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
-      if (!eqArrays(arr1[i], arr2[i])) {
-        return false;
-      }
-      continue;
-    }
-
-    if (typeof arr1[i] === 'object' && !Array.isArray(arr1[i]) && arr1[i] !== null && typeof arr2[i] === 'object' && !Array.isArray(arr2[i]) && arr2[i] !== null) {
-      if (!eqObjects(object1[key], object2[key])) {
-        return false;
-      }
-      continue;
-    }
-
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   let output = eqArrays(arr1, arr2) ? `✅✅✅ Assertion Passed: ${arr1} === ${arr2}` : `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`;
   console.log(output);
 };
+
+module.exports = assertArraysEqual;
